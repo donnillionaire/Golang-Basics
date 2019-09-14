8042a57256d11720ce58f2ad9f6e48859a2b651a
donnyllionaire <collinstheprogreammer@gmail.com>
2019-09-15 02:13:01 +0300

    functions with return values and recursion

--- functions.go
+++ functions.go
@@ -10,13 +10,41 @@ func sayGoodbye(name string){
   fmt.Println("Goodbye", name)
 }
 
+// Return Fuctions
+func addOne(x int) int {
+  return x + 1
+}
+
 func beSocial(name string){
   sayHello(name)
   fmt.Println("Hows the weather")
   sayGoodbye(name)
 }
+
+//Recursion:-Functions Can call themselves (making infinite loop)
+func sayAbunchofHellos(){
+  fmt.Println("hello")
+  sayAbunchofHellos()
+
+}
+
+
 // main fuction
 func main(){
   beSocial("Don")
   beSocial ("Collins")
+
+  //calling the return function (addOne)
+  x:= 5
+  x = addOne(x)
+  fmt.Println(x)
+
+  //Composing Fuctions
+  x = addOne(addOne(x))
+    fmt.Println(x)
+
+
+
+
+
 }
